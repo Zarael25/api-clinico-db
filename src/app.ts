aad385f92c7191f5e09466a3ee05d4e2f5b6886b
Zarael25 <telecom25a@gmail.com>
2025-10-06 21:29:18 -0400

    Despliegue: Se añadio url del backend

--- src/app.ts
+++ src/app.ts
@@ -63,7 +63,8 @@ const whitelist: string[] = [
   'http://localhost:3000',
   'http://192.168.1.101:4000',
   'http://localhost:4000',
-  'https://don-bosco-clinico.vercel.app'
+  'https://don-bosco-clinico.vercel.app',
+  'https://api-clinico-db.vercel.app'
 ]
 
 const options: cors.CorsOptions = {
